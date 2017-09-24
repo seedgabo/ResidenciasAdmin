a6c4f4dbb0544488da066df8f7a481cac83ec7bd
seedgabo <seedgabo@gmail.com>
2017-09-23 22:40:44 -0500

    seller

--- src/pages/seller/seller.ts
+++ src/pages/seller/seller.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, LoadingController, AlertController } from 'ionic-angular';
 import { Api } from '../../providers/api';
+
+import moment from 'moment';
 @Component({
   selector: 'page-seller',
   templateUrl: 'seller.html',
@@ -19,7 +21,7 @@ export class SellerPage {
   }];
   residences = [];
   residents = [];
-  constructor(public navCtrl: NavController, public navParams: NavParams, public api: Api) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public loading: LoadingController, public alert: AlertController, public api: Api) {
   }
 
   ionViewDidLoad() {
@@ -36,6 +38,9 @@ export class SellerPage {
   }
 
   getResidents() {
+    if (!this.charge.residence_id) {
+      return;
+    }
     this.api.get('residences/' + this.charge.residence_id + '?with[]=residents')
       .then((data: any) => {
         this.residents = data.residents;
@@ -55,7 +60,6 @@ export class SellerPage {
       amount: 0,
       quantity: 0,
     }];
-    this.residences = [];
     this.residents = [];
   }
   addItem() {
@@ -65,8 +69,62 @@ export class SellerPage {
       quantity: 0,
     })
   }
+
   removeItem(index) {
     this.items.splice(index, 1);
+  }
+
+  proccess() {
+    var procesing = 0;
+    var loading = this.loading.create({
+      content: this.api.trans('__.procesando') + procesing + '  de ' + this.items.length,
+    });
+    loading.present();
+    this.items.forEach(element => {
+      this.api.post('charges', {
+        'residence_id': this.charge.residence_id,
+        'concept': element.concept + "(x" + element.quantity + ")",
+        amount: element.amount * element.quantity,
+        month: moment().month() + 1,
+        year: moment().year(),
+        type: "unique",
+      })
+        .then(() => {
+          loading.setContent(this.api.trans('__.procesando') + ++procesing + '  de ' + this.items.length);
+          if (procesing == this.items.length) {
+            loading.dismiss();
+            this.complete();
+          }
+        })
+        .catch((err) => {
+          console.error(err);
+          loading.dismiss();
+          this.alert.create({
+            title: "ERROR",
+            message: JSON.stringify(err)
+          }).present();
+        })
+    });
+
+  }
+
+  complete() {
+    this.alert.create({
+      message: this.api.trans('literals.ready'),
+      buttons: ["OK"]
+    }).present();
+    this.clear();
+  }
+
+  canProccess() {
+    var valid = true;
+    this.items.forEach((item) => {
+      if (!(item.amount > 0 && item.quantity > 0 && item.concept.length > 0))
+        valid = false;
+    });
+    return this.charge.residence_id != null
+      && this.items.length > 0
+      && valid;
   }
 
 }
