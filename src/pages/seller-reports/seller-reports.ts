abaf4dd37d5c2127ef8a7d7158f95e52a366c990
gabriel <seedgabo@gmail.com>
2017-11-24 09:53:36 -0500

    cancelar invoice

--- src/pages/seller-reports/seller-reports.ts
+++ src/pages/seller-reports/seller-reports.ts
@@ -84,6 +84,8 @@ export class SellerReportsPage {
     }, 300);
   }
 
+
+
   actions(invoice) {
     var sheet = this.actionsheet.create({
       title: this.api.trans("literals.actions") + " " + this.api.trans('literals.invoice'),
@@ -91,10 +93,21 @@ export class SellerReportsPage {
 
     sheet.addButton({
       text: this.api.trans('literals.print'),
+      icon : 'print',
       handler: () => {
         this.printInvoice(invoice)
       }
     })
+    
+    if(invoice.status !== 'cancelled'){
+      sheet.addButton({
+        text: this.api.trans('crud.cancel') + " " + this.api.trans('literals.invoice'),
+        icon: 'remove-circle',
+        handler: () => {
+          this.cancelInvoice(invoice)
+        }
+      })
+    }
 
 
     sheet.addButton({
@@ -104,6 +117,33 @@ export class SellerReportsPage {
       handler: () => {
       }
     }).present();
+  }
+
+  cancelInvoice(invoice) {
+      this.api.alert.create({
+        title: this.api.trans("__.nota de cancelacion"),
+        inputs: [{
+          label: this.api.trans('literals.note'),
+          placeholder: this.api.trans('literals.note'),
+          name: "note",
+          type: "text",
+        }],
+        buttons : [{
+          text : this.api.trans("literals.proccess"),
+          handler: (data)=>{
+              if(data.note){
+                this.api.put(`invoices/${invoice.id}`,{status:"cancelled", note: data.note})
+                .then((resp)=>{
+                    invoice.status = "cancelled"
+                    invoice.note = data.note
+                })
+                .catch((err)=>{
+                  this.api.Error(err);
+                })
+              }
+          }
+        }]
+      }).present()
   }
 
   more() {
