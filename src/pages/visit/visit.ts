56fdeab8a46fe8d1bf40f0a020e14eb95dd28647
gabriel <seedgabo@gmail.com>
2018-01-31 08:02:15 -0500

    visitors

--- src/pages/visit/visit.ts
+++ src/pages/visit/visit.ts
@@ -15,7 +15,7 @@ export class VisitPage {
   }
 
   ionViewDidLoad() {
-    this.api.get(`visits/${this.visit.id}?with[]=visitor&with[]=visitors&with[]=vehicle&with[]=parking&with[]=user&with[]=residence&append[]=guest`)
+    this.api.get(`visits/${this.visit.id}?with[]=visitor&with[]=visitors&with[]=vehicle&with[]=parking&with[]=creator&with[]=residence&append[]=guest`)
       .then((data) => {
         this.visit = data;
       })
