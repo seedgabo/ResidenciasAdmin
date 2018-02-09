0eea510eba9419883b8a01da8db4986f131b9133
seedgabo <seedgabo@gmail.com>
2018-02-09 18:33:00 -0500

    access control finish

--- src/pages/lobby/lobby.ts
+++ src/pages/lobby/lobby.ts
@@ -3,8 +3,8 @@ import { VisitCreatorPage } from './../visit-creator/visit-creator';
 import { VisitorPage } from './../visitor/visitor';
 import { Api } from './../../providers/api';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ToastController, ActionSheetController, ModalController } from 'ionic-angular';
-
+import { IonicPage, NavController, NavParams, ToastController, ActionSheetController, ModalController, AlertController, Events } from 'ionic-angular';
+import * as moment from 'moment';
 @IonicPage()
 @Component({
   selector: 'page-lobby',
@@ -25,27 +25,40 @@ export class LobbyPage {
    * Fingerprint integration
    * Register entries for worker and users
   **/
-  constructor(public navCtrl: NavController, public navParams: NavParams, public api: Api, public modal: ModalController, public toast: ToastController, public actionsheet: ActionSheetController) {
+  handler = () => {
+    this.getVisits()
+  }
+  constructor(public navCtrl: NavController, public navParams: NavParams, public api: Api, public modal: ModalController, public toast: ToastController, public actionsheet: ActionSheetController, public alert: AlertController, public events: Events) {
+  }
+
+  ionViewDidLoad() {
+    this.events.subscribe("VisitCreated", this.handler);
+    this.events.subscribe("VisitUpdated", this.handler);
+    this.events.subscribe("VisitDeleted", this.handler);
+  }
+
+  ionViewDidLeave() {
+    this.events.unsubscribe("VisitCreated", this.handler);
+    this.events.unsubscribe("VisitUpdated", this.handler);
+    this.events.unsubscribe("VisitDeleted", this.handler);
+  }
+
+  // Lobby Functions
+  ionViewDidEnter() {
     this.api.ready.then(() => {
       var promises = [
         this.api.load("users"),
         this.api.load("visitors"),
         this.api.load("workers"),
       ]
-      Promise.all(promises).then(() => { this.ready = true })
+      Promise.all(promises).then(() => {
+        this.searchPerson()
+        this.getVisits()
+        this.getVisitors()
+        this.ready = true
+
+      })
     })
-  }
-
-  ionViewDidLoad() {
-
-  }
-
-  ionViewDidEnter() {
-    if (this.ready) {
-      this.searchPerson()
-      this.getVisits()
-      this.getVisitors()
-    }
   }
 
   searchPerson() {
@@ -120,83 +133,6 @@ export class LobbyPage {
     });
   }
 
-  clear() {
-    this.person = null
-    this.no_results = false
-  }
-
-
-  getVisits() {
-    this.api.get('visits?with[]=residence&with[]=user&with[]=vehicle&with[]=visitor&withCount[]=visitors' + this.append()).then((data: any) => {
-      console.log(data);
-      this.visits = this._visits = data;
-      this.filter()
-    }).catch((err) => {
-      console.error(err);
-    });
-  }
-
-  getVisitors() {
-    if (this.query == "")
-      return this.visitors = this.api.objects.visitors.slice(0, 100);
-
-    return this.visitors = this.api.objects.visitors.filter((visitor) => {
-      if (visitor.name.toLowerCase().indexOf(this.query.toLowerCase()) > -1
-        || visitor.document.toLowerCase().indexOf(this.query.toLowerCase()) > -1)
-        return true;
-      if (this.api.residences_collection[visitor.residence_id] && this.api.residences_collection[visitor.residence_id].name.toLowerCase().indexOf(this.query.toLowerCase()) > -1)
-        return true;
-      return false;
-    }).slice(0, 100);
-  }
-
-  filter() {
-    if (this.query == "") {
-      return this.visits = this._visits.data;
-    }
-    var array = [];
-    for (var index = 0; index < this._visits.data.length; index++) {
-      var element = this._visits.data[index];
-      if (
-        (element.visitor && element.visitor.name.toLowerCase().indexOf(this.query.toLowerCase()) !== -1) ||
-        (element.visitor && element.visitor.document && element.visitor.document.toLowerCase().indexOf(this.query.toLowerCase()) !== -1) ||
-        (element.guest && element.guest.name.toLowerCase().indexOf(this.query.toLowerCase()) !== -1) ||
-        (element.residence && element.residence.name.toLowerCase().indexOf(this.query.toLowerCase()) !== -1) ||
-        (element.user && element.user.name.toLowerCase().indexOf(this.query.toLowerCase()) !== -1)
-      )
-        array[array.length] = element;
-
-      if (array.length == 200) {
-        break;
-      }
-    }
-    console.log(array);
-    return this.visits = array;
-  }
-
-  append() {
-    var append = "&append[]=guest&order[id]=desc&paginate=500";
-    return append;
-  }
-
-
-  addVisit(visitor) {
-    this.loading = true
-    var visit: any = {
-      status: "waiting for confirmation",
-      visitor_id: visitor.id,
-      residence_id: visitor.residence_id
-    }
-    this.api.post(`visitors/${visitor.id}/visit`, visit).then((response) => {
-      console.log(response);
-      this.done()
-      this.loading = false;
-    }).catch((err) => {
-      this.api.Error(err)
-      this.loading = false;
-    });
-  }
-
   findPerson() {
     var modal = this.modal.create('PersonFinderPage', {
       users: true,
@@ -216,17 +152,17 @@ export class LobbyPage {
 
   }
 
-  canSave() {
-    return this.person.name && this.person.name.length > 3 && this.person.document && this.person.residence_id;
+  clear() {
+    this.person = null
+    this.no_results = false
   }
 
-  private done() {
-    this.toast.create({
-      message: this.api.trans("literals.done"),
-      duration: 3000
-    }).present();
-    this.person = null;
-    this.type = null;
+  gotoVisits() {
+    this.navCtrl.push('ListPage');
+  }
+
+  gotoVisitors() {
+    this.navCtrl.push('HomePage');
   }
 
   more(v) {
@@ -263,40 +199,13 @@ export class LobbyPage {
     }).present();
   }
 
-
-  visitorModal(visitor = null) {
-    var residence
-    if (visitor && visitor.residence) {
-      residence = visitor.residence
-    }
-    var modal = this.modal.create(VisitorPage, { visitor: visitor, residence: residence }, { showBackdrop: true, enableBackdropDismiss: true })
-    modal.present();
-    modal.onDidDismiss((data) => {
-      if (data) {
-        this.type = "visitor"
-        this.person = data;
-      }
-    })
-  }
-
-  visitModal(visitor = undefined) {
-    this.modal.create(VisitCreatorPage, { visitor: visitor }, { showBackdrop: true, enableBackdropDismiss: true }).present();
-
-  }
-
-
-  viewVisit(visit, index) {
-    this.navCtrl.push(VisitPage, {
-      visit: visit,
-      done: () => {
-        this.dismissPreApproved(visit, index)
-      }
-    });
-  }
-
-  dismissPreApproved(visit, index) {
-    this.api.visits_approved.splice(index, 1);
-    this.api.storage.set('visits_approved', this.api.visits_approved);
+  private done() {
+    this.toast.create({
+      message: this.api.trans("literals.done"),
+      duration: 3000
+    }).present();
+    this.person = null;
+    this.type = null;
   }
 
 
@@ -333,12 +242,283 @@ export class LobbyPage {
   }
 
 
+  // Visits Funcions
+  getVisits() {
+    this.api.get('visits?with[]=residence&with[]=user&with[]=vehicle&with[]=visitor&withCount[]=visitors' + this.append()).then((data: any) => {
+      console.log(data);
+      this.visits = this._visits = data;
+      this.filter()
+    }).catch((err) => {
+      console.error(err);
+    });
+  }
+
+  filter() {
+    if (this.query == "") {
+      return this.visits = this._visits.data;
+    }
+    var array = [];
+    for (var index = 0; index < this._visits.data.length; index++) {
+      var element = this._visits.data[index];
+      if (
+        (element.visitor && element.visitor.name.toLowerCase().indexOf(this.query.toLowerCase()) !== -1) ||
+        (element.visitor && element.visitor.document && element.visitor.document.toLowerCase().indexOf(this.query.toLowerCase()) !== -1) ||
+        (element.guest && element.guest.name.toLowerCase().indexOf(this.query.toLowerCase()) !== -1) ||
+        (element.residence && element.residence.name.toLowerCase().indexOf(this.query.toLowerCase()) !== -1) ||
+        (element.user && element.user.name.toLowerCase().indexOf(this.query.toLowerCase()) !== -1)
+      )
+        array[array.length] = element;
+
+      if (array.length == 200) {
+        break;
+      }
+    }
+    console.log(array);
+    return this.visits = array;
+  }
+
+  append() {
+    var append = "&append[]=guest&order[id]=desc&paginate=500";
+    return append;
+  }
+
+  addVisit(visitor) {
+    this.loading = true
+    var visit: any = {
+      status: "waiting for confirmation",
+      visitor_id: visitor.id,
+      residence_id: visitor.residence_id
+    }
+    this.api.post(`visitors/${visitor.id}/visit`, visit).then((response) => {
+      console.log(response);
+      this.done()
+      this.loading = false;
+    }).catch((err) => {
+      this.api.Error(err)
+      this.loading = false;
+    });
+  }
+
+  actions_visit(visit) {
+    var buttons: any = [{
+      text: this.api.trans('crud.add') + " " + this.api.trans('literals.note'),
+      icon: 'create',
+      handler: () => { this.addNote(visit) }
+    }];
+    if (visit.status == 'waiting for confirmation') {
+      buttons.push({
+        text: this.api.trans('__.approve'),
+        icon: 'checkbox',
+        cssClass: 'icon-secondary',
+        handler: () => { this.approve(visit) }
+      })
+    }
+    if (visit.status != 'departured') {
+      buttons.push({
+        text: this.api.trans('__.set departure'),
+        icon: 'log-out',
+        cssClass: 'icon-primary',
+        handler: () => { this.departure(visit); }
+      })
+    }
+
+    buttons.push({
+      text: this.api.trans('literals.view_resource') + " " + this.api.trans('literals.visit'),
+      icon: 'eye',
+      // cssClass: 'icon-danger',
+      handler: () => { this.viewVisit(visit) }
+    });
+    if (visit.visitor)
+      buttons.push({
+        text: this.api.trans('crud.edit') + " " + this.api.trans('literals.visitor'),
+        icon: 'contact',
+        handler: () => { this.editVisitor(visit.visitor) }
+      });
+
+    buttons.push({
+      text: this.api.trans('crud.delete') + " " + this.api.trans('literals.visit'),
+      icon: 'trash',
+      cssClass: 'icon-danger',
+      role: 'destructive',
+      handler: () => { this.deleteVisit(visit) }
+    });
 
 
-  gotoVisits() {
-    this.navCtrl.push('ListPage');
+
+    this.actionsheet.create({
+      title: this.api.trans('literals.visit') + " " + this.api.trans('__.from') + " " +
+        (visit.visitor ? visit.visitor.name : visit.guest ? visit.guest.name : ''),
+      buttons: buttons
+    }).present();
   }
-  gotoVisitors() {
-    this.navCtrl.push('HomePage');
+
+  addNote(visit) {
+    this.alert.create({
+      title: this.api.trans("crud.add") + " " + this.api.trans('literals.note'),
+      inputs: [{
+        name: 'note',
+        placeholder: this.api.trans('literals.note'),
+        value: visit.note
+      }],
+      buttons: [{
+        text: 'OK',
+        handler: (data) => {
+          if (data && data.note) {
+            this.api.put('visits/' + visit.id, { note: data.note })
+              .then((resp) => {
+                visit.note = data.note
+              })
+              .catch((err) => {
+                this.api.Error(err)
+              })
+          }
+        }
+      }, {
+        text: this.api.trans('crud.cancel')
+      }]
+    }).present();
   }
+
+
+  approve(visit) {
+    this.api.put('visits/' + visit.id, { status: 'approved', departured_at: moment.utc().toString() })
+      .then((data) => {
+        console.log(data);
+        visit.status = 'approved';
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  departure(visit) {
+    this.api.put('visits/' + visit.id, { status: 'departured', departured_at: moment.utc().toString() })
+      .then((data) => {
+        console.log(data);
+        visit.status = 'departured';
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  editVisitor(visitor = null) {
+    this.modal.create(VisitorPage, { visitor: visitor }, { showBackdrop: true, enableBackdropDismiss: true }).present();
+  }
+
+  deleteVisit(visit) {
+    this.api.delete('visits/' + visit.id)
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+
+  }
+
+  viewVisit(visit) {
+    this.navCtrl.push(VisitPage, {
+      visit: visit,
+      done: () => {
+        this.api.put("visits/" + visit.id, { status: "departured" })
+          .then(() => {
+            visit.status = "departured";
+
+          })
+          .catch(console.error)
+      }
+    });
+  }
+
+  errorHandler(event) {
+    console.debug(event);
+    event.target.src = "https://cdn.browshot.com/static/images/not-found.png";
+  }
+
+  dismissPreApproved(visit, index) {
+    this.api.visits_approved.splice(index, 1);
+    this.api.storage.set('visits_approved', this.api.visits_approved);
+  }
+
+
+
+  // Visitors
+  getVisitors() {
+    if (this.query == "")
+      return this.visitors = this.api.objects.visitors.slice(0, 100);
+
+    return this.visitors = this.api.objects.visitors.filter((visitor) => {
+      if (visitor.name.toLowerCase().indexOf(this.query.toLowerCase()) > -1
+        || visitor.document.toLowerCase().indexOf(this.query.toLowerCase()) > -1)
+        return true;
+      if (this.api.residences_collection[visitor.residence_id] && this.api.residences_collection[visitor.residence_id].name.toLowerCase().indexOf(this.query.toLowerCase()) > -1)
+        return true;
+      return false;
+    }).slice(0, 100);
+  }
+
+  actions_visitor(visitor) {
+    this.actionsheet.create({
+      title: this.api.trans('literals.actions') + " | " + visitor.name,
+      buttons: [
+        {
+          text: this.api.trans('literals.generate') + " " + this.api.trans('literals.visit'),
+          icon: 'person-add',
+          cssClass: 'icon-primary',
+          handler: () => { this.visitModal(visitor) }
+        },
+        {
+          text: this.api.trans('crud.edit'),
+          icon: 'create',
+          cssClass: 'icon-warning',
+          handler: () => { this.visitorModal(visitor) }
+        },
+        {
+          text: this.api.trans('crud.delete'),
+          icon: 'trash',
+          role: 'destructive',
+          cssClass: 'icon-danger',
+          handler: () => { this.deleteVisitor(visitor) }
+        }
+
+      ]
+    }).present();
+  }
+
+  deleteVisitor(visitor) {
+    this.api.delete('visitors/' + visitor.id).then((data) => {
+      console.log(data);
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
+
+  visitorModal(visitor = null) {
+    var residence
+    if (visitor && visitor.residence) {
+      residence = visitor.residence
+    }
+    var modal = this.modal.create(VisitorPage, { visitor: visitor, residence: residence }, { showBackdrop: true, enableBackdropDismiss: true })
+    modal.present();
+    modal.onDidDismiss((data) => {
+      if (data) {
+        this.type = "visitor"
+        this.person = data;
+      }
+    })
+  }
+
+  visitModal(visitor = undefined) {
+    this.modal.create(VisitCreatorPage, { visitor: visitor }, { showBackdrop: true, enableBackdropDismiss: true }).present();
+
+  }
+
+  canSave() {
+    return this.person.name && this.person.name.length > 3 && this.person.document && this.person.residence_id;
+  }
+
+
+
+
 }
